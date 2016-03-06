726441fb04e43b53bb15433d87a9de86c2f421b1
dylanka <Dylan@Dylans-MacBook-Air.local>
2016-03-06 18:00:56 -0500

    game

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,113 +1,180 @@
-var game = new Phaser.Game(600, 480, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-function preload() {
+        var mainState;
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/ground.png');
-    game.load.image('ship', 'assets/ship.png', 32, 48);
+        var background;
+        var player;
+        var cursors;
+        var bullets;
+        var bulletTime = 0;
+        var fire;
+        var explosion;
 
-        for (var i = 1; i <= 11; i++)
-    {
-        game.load.image('bullet' + i, 'assets/bullet' + i + '.png');
-    }
+        var enemies;
 
-}
+        var score = 0;
+        var scoreString = '';
+        var scoreText;
+        var winText;
 
-function create() {
-}
+        function preload() {
 
-function update() {
+            game.load.image('background', 'assets/background.png');
+            game.load.image('player', 'assets/ship.png');
+            game.load.image('bullet', 'assets/bullet.png');
+            game.load.image('enemy', 'assets/enemy.png');
+            game.load.spritesheet('explosion', 'assets/explode.png', 128, 128);
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    game.add.sprite(0, 0, 'sky');
-
-
-}
-
-    var Bullet = function (game, key) {
-
-        Phaser.Sprite.call(this, game, 0, 0, key);
-
-        this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
-
-        this.anchor.set(0.5);
-
-        this.checkWorldBounds = true;
-        this.outOfBoundsKill = true;
-        this.exists = false;
-
-        this.tracking = false;
-        this.scaleSpeed = 0;
-
-    };
-
-    Bullet.prototype = Object.create(Phaser.Sprite.prototype);
-    Bullet.prototype.constructor = Bullet;
-
-    Bullet.prototype.fire = function (x, y, angle, speed, gx, gy) {
-
-        gx = gx || 0;
-        gy = gy || 0;
-
-        this.reset(x, y);
-        this.scale.set(1);
-
-        this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
-
-        this.angle = angle;
-
-        this.body.gravity.set(gx, gy);
-
-    };
-
-    Bullet.prototype.update = function () {
-
-        if (this.tracking)
-        {
-            this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
         }
 
-        if (this.scaleSpeed > 0)
-        {
-            this.scale.x += this.scaleSpeed;
-            this.scale.y += this.scaleSpeed;
+        function create() {
+
+            game.add.sprite(0, 0, 'background');
+
+            background = game.add.tileSprite(0, 0, 800, 800, 'background');
+
+            player = game.add.sprite(100, game.world.height - 300, 'player');
+
+            game.physics.enable(player, Phaser.Physics.ARCADE);
+
+            player.body.collideWorldBounds=true;
+
+            cursors = game.input.keyboard.createCursorKeys();
+
+            bullets = game.add.group();
+            bullets.enableBody = true;
+            bullets.physicsBodyType = Phaser.Physics.ARCADE;
+            bullets.createMultiple(1000, 'bullet');
+            bullets.setAll('anchor.x', 1);
+            bullets.setAll('anchor.y', 0.5);
+            bullets.setAll('outOfBoundsKill', true);
+            bullets.setAll('checkWorldBounds', true);
+
+            fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+            enemies = game.add.group();
+            enemies.enableBody = true;
+            enemies.physicsBodyType = Phaser.Physics.ARCADE;
+
+            // explosions
+            explosions = game.add.group();
+            explosions.createMultiple(20, 'explosion');
+            explosions.forEach(setupEnemies, this);
+
+            createEnemies();
+
+            function createEnemies(){
+                for(var x = 0; x < 4; x++){
+                    for(var y = 0; y < 5; y++){
+                        var enemy = enemies.create(x*70,y*75,'enemy');
+                        enemy.anchor.setTo(0.5,0.5);
+                    }
+                }
+
+                enemies.y = 100;
+                enemies.x = 900;
+
+                // Enemy movement loop
+                var tween = game.add.tween(enemies).to({x:-350}, 5000, Phaser.Easing.Linear.None, true, 0, 500, false);
+
+                tween.onLoop.add(attack,this);
+
+            }
+
+            function setupEnemies(enemy){
+                enemy.anchor.x = 0.5;
+                enemy.anchor.y = 0.5;
+                enemy.animations.add('explosion');
+            }
+
+            function attack(){
+                enemies.x += 8;
+            }            
+
+            scoreString = 'Score : ';
+            scoreText = game.add.text(10, 10, 'Score: 0',{fontsize: '32px', fill: '#fff'});
+            
+            // winText = game.add.text(game.world.centerX,game.world.centerY,'You Win!'{fontsize: '32px', fill: '#fff'});
+            // winText.anchor.setTo(0.5, 0.5);
+            // winText.visible = false;
+
         }
 
-    };
+        function update() {
 
-    var Weapon = {};
+            // player starting speed
+            player.body.velocity.x = 0;
 
-    Weapon.SingleBullet = function (game) {
+            player.body.velocity.y = 0;
 
-        Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
+            // background loop
+            background.tilePosition.x -= 1;
 
-        this.nextFire = 0;
-        this.bulletSpeed = 600;
-        this.fireRate = 100;
+            // key commands
+            if(cursors.left.isDown){
+                player.body.velocity.x = -180;
+            }
+            if(cursors.right.isDown){
+                player.body.velocity.x = 180;
+            }
+            if(cursors.up.isDown){
+                player.body.velocity.y = -180;
+            }
+            if(cursors.down.isDown){
+                player.body.velocity.y = 180;
+            }
 
-        for (var i = 0; i < 64; i++)
-        {
-            this.add(new Bullet(game, 'bullet5'), true);
+            if(fireButton.isDown){
+                fireBullet();
+            }
+
+            // bullet to enemy physics
+            game.physics.arcade.overlap(bullets,enemies,collisionHandler,null,this);
+            // player to enemy physics
+            game.physics.arcade.overlap(enemies,player,enemyHitsPlayer,null,this);
+
+            if(score == 2000){
+                winText.visible = true;
+                scoreText.visible = false;
+            }
+
         }
 
-        return this;
+        // bullet firing speed and function
+        function fireBullet(){
+            if(game.time.now > bulletTime)
+                bullet = bullets.getFirstExists(false);
+                    if(bullet){
+                        // controls bullet firing place
+                        bullet.reset(player.x + 25,player.y + 10);
+                        bullet.body.velocity.x = 500;
+                        bulletTime = game.time.now + 40;
+                    }
+        }
 
-    };
+        //bullet collision 
+        function collisionHandler(bullet,enemy){
+            bullet.kill();
+            enemy.kill();
+            score += 100;
+            scoreText.text = scoreString + score;
 
-    Weapon.SingleBullet.prototype = Object.create(Phaser.Group.prototype);
-    Weapon.SingleBullet.prototype.constructor = Weapon.SingleBullet;
+            // enemy explodes
+            var explosion = explosions.getFirstExists(false);
+            explosion.reset(enemy.body.x, enemy.body.y);
+            explosion.play('explosion', 20, false, true);
+        }
 
-    Weapon.SingleBullet.prototype.fire = function (source) {
+        // player enemy collision
+        function enemyHitsPlayer (player,enemy){
+            player.kill();
+            enemy.kill();
+            // player explodes
+            var explosion = explosions.getFirstExists(false);
+            explosion.reset(player.body.x, player.body.y);
+            explosion.play('explosion', 20, false, true);            
+        }
 
-        if (this.game.time.time < this.nextFire) { return; }
 
-        var x = source.x + 10;
-        var y = source.y + 10;
-
-        this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
-
-        this.nextFire = this.game.time.time + this.fireRate;
-
-    };
-
+        game.state.add('mainState',mainState);
+        game.state.start('mainState');
